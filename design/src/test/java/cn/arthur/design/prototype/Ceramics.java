d65b7c3a3f1cbfdebdc71004d9bb38a0298f1ba1
梅俊华 <meijunhua@worken.cn>
2021-04-30 23:09:20 +0800

    原型模式

--- design/src/test/java/cn/arthur/design/prototype/Ceramics.java
+++ design/src/test/java/cn/arthur/design/prototype/Ceramics.java
@@ -6,18 +6,20 @@ package cn.arthur.design.prototype;
  * @author : arthur
  * @date : 2021/3/23 0023 22:21
  */
-public class Ceramics implements Cloneable{
+public class Ceramics implements Cloneable {
     private String body;
 
-   public void setBody(String body){
-       this.body=body;
-   }
-   public String getBody(){
-       return body;
-   }
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getBody() {
+        return body;
+    }
 
     @Override
     public Ceramics clone() throws CloneNotSupportedException {
-        return (Ceramics)super.clone();
-   }
+        //深克隆
+        return (Ceramics) super.clone();
+    }
 }
