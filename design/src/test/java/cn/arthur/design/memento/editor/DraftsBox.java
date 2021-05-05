1c6ccc45d0380a95ce0e9bb1de4d78d36a7c77f1
梅俊华 <meijunhua@worken.cn>
2021-05-05 13:15:43 +0800

    备忘录模式

---
+++ design/src/test/java/cn/arthur/design/memento/editor/DraftsBox.java
@@ -0,0 +1,20 @@
+package cn.arthur.design.memento.editor;
+
+import java.util.Stack;
+
+/**
+ * Created by Tom.
+ */
+public class DraftsBox {
+    private final Stack<ArticleMemento> STACK = new Stack<ArticleMemento>();
+
+    public ArticleMemento getMemento(){
+        ArticleMemento articleMemento = STACK.pop();
+        return articleMemento;
+    }
+
+    public void addMemento(ArticleMemento articleMemento){
+        STACK.push(articleMemento);
+    }
+
+}
